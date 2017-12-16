c971570c7f1aa90471a66682f02dee4a675d24cb
sbam1 <bamsuseel@gmail.com>
2017-12-16 14:23:49 -0500

    adding baseic spring security configuration.

---
+++ src/main/java/suseel/java/mind/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package suseel.java.mind.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
